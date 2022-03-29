632f1fca44310ffa2f10e8d2e934e410bd31a637
safronman <safronmanbox@gmail.com>
2022-03-29 14:28:48 +0300

    develop/login branch commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
             <div>Dev branch commit 1</div>
             <div>Dev branch commit 2</div>
             <div>Dev branch commit 3</div>
+            <div>develop/login branch commit 1</div>
         </div>
     );
 }
