5fbd03a675b698a116a6be143c1d453f07b0a93e
safronman <safronmanbox@gmail.com>
2022-03-29 14:31:50 +0300

    develop/popup branch commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
             <div>Dev branch commit 2</div>
             <div>Dev branch commit 3</div>
             <div>develop/popup branch commit 1</div>
+            <div>develop/popup branch commit 2</div>
         </div>
     );
 }
